9beef2addd8c834dfde970e33d4cc776e63489fd
Azure <aaazureee@gmail.com>
2017-08-22 08:48:37 +1000

    fix tab size = 2

--- dotabuff.js
+++ dotabuff.js
@@ -1,62 +1,62 @@
 $(() => {
-    var api_key = "040FA36AB25776E7072B0B42CAB25A20";
-    $(document).keypress(e => {
-        var key = e.which;
-        if (key == 13) // the enter key code
-        {
-            $('#test').click();
-            return false;
-        }
-    });
-    $("#test").click(() => {
-        var div = "<div id='info'><img alt='loading' src='https://media.giphy.com/media/y1ZBcOGOOtlpC/giphy.gif'></div>";
-        $(".output").html(div);
-        var input = $("input").val();
-        // https/...../
-        if (input.slice(-1) === '/') {
-            input = input.slice(0, -1).replace(/.*\//, "");
-        } else if (input.includes('/')) {
-            input = input.replace(/.*\//, "");
-        }
+	var api_key = "040FA36AB25776E7072B0B42CAB25A20";
+	$(document).keypress(e => {
+		var key = e.which;
+		if (key == 13) // the enter key code
+		{
+			$('#test').click();
+			return false;
+		}
+	});
+	$("#test").click(() => {
+		var div = "<div id='info'><img alt='loading' src='https://media.giphy.com/media/y1ZBcOGOOtlpC/giphy.gif'></div>";
+		$(".output").html(div);
+		var input = $("input").val();
+		// https/...../
+		if (input.slice(-1) === '/') {
+			input = input.slice(0, -1).replace(/.*\//, "");
+		} else if (input.includes('/')) {
+			input = input.replace(/.*\//, "");
+		}
 
-        if (isNaN(input)) {
-            $.ajax({
-                    url: `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${api_key}&vanityurl=${input}`,
-                    dataType: 'json'
-                })
-                .done(data => {
-                    var id_64bit = data.response.steamid;
-                    if (!id_64bit) {
-                        $(".output").html(`Not found 4Head`);
-                    } else {
-                        var id_32bit = convert(id_64bit);
-                        $(".output").html(`<a href = "https://www.dotabuff.com/players/${id_32bit}">Dotabuff</a>`);
-                        $("a").attr("target", "_blank");
+		if (isNaN(input)) {
+			$.ajax({
+					url: `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${api_key}&vanityurl=${input}`,
+					dataType: 'json'
+				})
+				.done(data => {
+					var id_64bit = data.response.steamid;
+					if (!id_64bit) {
+						$(".output").html(`Not found 4Head`);
+					} else {
+						var id_32bit = convert(id_64bit);
+						$(".output").html(`<a href = "https://www.dotabuff.com/players/${id_32bit}">Dotabuff</a>`);
+						$("a").attr("target", "_blank");
 
-                    }
-                });
-        } else {
-            $.ajax({
-                    url: `https://cors-anywhere.herokuapp.com/http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${api_key}&steamids=${input}`,
-                    dataType: 'json'
-                })
-                .done(data => {
-                    if (!data.response.players[0]) {
-                        $(".output").html(`Not found 4Head`);
-                    } else {
-                        var id_32bit = convert(input);
-                        $(".output").html(`<a href = "https://www.dotabuff.com/players/${id_32bit}">Dotabuff</a>`);
-                        $("a").attr("target", "_blank");
+					}
+				});
+		} else {
+			$.ajax({
+					url: `https://cors-anywhere.herokuapp.com/http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${api_key}&steamids=${input}`,
+					dataType: 'json'
+				})
+				.done(data => {
+					if (!data.response.players[0]) {
+						$(".output").html(`Not found 4Head`);
+					} else {
+						var id_32bit = convert(input);
+						$(".output").html(`<a href = "https://www.dotabuff.com/players/${id_32bit}">Dotabuff</a>`);
+						$("a").attr("target", "_blank");
 
-                    }
-                });
-        }
-    });
+					}
+				});
+		}
+	});
 
-    function convert(bit64) {
-        var big = bigInt(bit64);
-        var minus = bigInt('76561197960265728');
-        var result = big.subtract(minus);
-        return result.value;
-    }
+	function convert(bit64) {
+		var big = bigInt(bit64);
+		var minus = bigInt('76561197960265728');
+		var result = big.subtract(minus);
+		return result.value;
+	}
 });
